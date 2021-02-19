633fede19941a8b8607deda98bd7fcda72b7f5e4
Felicia Wootton <felicia.wootton@gmail.com>
2021-02-18 18:44:37 -0600

    Added confirm message for upper case letters

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var numberOfChar = prompt("How many characters would you like in your password?");
-}
+
+  var numberOfChar = prompt("How many characters would you like in your password? (Choose a number 8 through 128)");
+
+  if (numberOfChar < 8 || numberOfChar > 128) {
+    alert("Not a valid number. Please choose a number 8 through 128.");
+  }
+
+  else if (numberOfChar >= 8 && numberOfChar <= 128) {
+  var upperCaseAnwer = confirm("Would you like upper case letters in your password?");
+    }
+    
+  }
+
+
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
 
