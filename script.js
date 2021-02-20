e5d5a8996959adf6a42f9ac1c1b486c836ae1434
Felicia Wootton <felicia.wootton@gmail.com>
2021-02-20 14:44:59 -0600

    Added conditions

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', '
 var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var password = [];
+var passwordArray = [];
 
 // Write password to the #password input
 function writePassword() {
-
+  var password = "";
   var numberOfChar = prompt("How many characters would you like in your password? (Choose a number 8 through 128)");
 
   if (numberOfChar < 8 || numberOfChar > 128) {
@@ -18,25 +18,46 @@ function writePassword() {
   }
 
   else if (numberOfChar >= 8 && numberOfChar <= 128) {
-  var upperCaseAnswer = confirm("Would you like upper case letters in your password?");
-  var lowerCaseAnswer = confirm("Would you like lower case letters in your password?");
-  var specialCharAnswer = confirm("Would you like special characters in your password?");
+    var upperCaseAnswer = confirm("Would you like upper case letters in your password?");
+    if (upperCaseAnswer) {
+      passwordArray.concat(upperCaseLetters);
     }
 
+    var lowerCaseAnswer = confirm("Would you like lower case letters in your password?");
+
+    var specialCharAnswer = confirm("Would you like special characters in your password?");
+
+    if (passwordArray.length == 0) {
+      alert("must")
+    }
+    else{
+      for (let index = 0; index < parseInt(numberOfChar); index++) {
+        //look for random index usinmg mathl.random
+        //append to password string :)
+        const element = passwordArray[index];
+        
+      }
+    }
+
+
+  }
+
+
+
   if (upperCaseAnswer === true && lowerCaseAnswer === true && specialCharAnswer === true) {
-  //   for (var i = 0; i < numberOfChar.length; i++) {
-  //     password.push(upperCaseLetters[Math.floor(Math.random() * 26)]);
-  //     console.log(password);
-  // }
+    //   for (var i = 0; i < numberOfChar.length; i++) {
+    //     password.push(upperCaseLetters[Math.floor(Math.random() * 26)]);
+    //     console.log(password);
+    // }
     console.log("It is working");
   }
-  }
+}
 
 
-  // var password = generatePassword();
-  // var passwordText = document.querySelector("#password");
+// var password = generatePassword();
+// var passwordText = document.querySelector("#password");
 
-  // passwordText.value = password;
+// passwordText.value = password;
 
 
 // Add event listener to generate button
