78fa26b142f2350e39fedd3f5e476e7eed473096
Felicia Wootton <felicia.wootton@gmail.com>
2021-02-21 09:10:43 -0600

    Worked on for loop

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 
+var specialChars = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var passwordArray = [];
+
+var password = "";
 
 // Write password to the #password input
 function writePassword() {
-
-  var specialChars = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-  var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-  var passwordArray = [];
-
-  // var password = "";
 
   var numberOfChar = prompt("How many characters would you like in your password? (Choose a number 8 through 128)");
 
@@ -44,24 +43,19 @@ function writePassword() {
 
     if (specialCharAnswer) {
       passwordArray = passwordArray.concat(specialChars);
-      console.log(password.Array);
     }
 
     if (passwordArray.length == 0) {
-      alert("Must choose at least one of the following: uppercase letters, lowercase letters, numbers, or special characters.")
+      alert("Must choose at least ONE of the following: uppercase letters, lowercase letters, numbers, or special characters.")
     }
 
     else{
       for (var index = 0; index < parseInt(numberOfChar); index++) {
-        password = password.append(passwordArray[Math.floor(Math.random() * (passwordArray.length - 1))]);
-        //look for random index usinmg mathl.random
-        //append to password string
-        // const element = passwordArray[index];
-        
+        var randomElement = passwordArray[Math.floor(Math.random() * (passwordArray.length - 1))];
+        password = password.append(randomElement);
+        console.log(password);
       }
     }
-
-
   }
   }
 
